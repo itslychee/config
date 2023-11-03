309700495c2dd7511b8f237a7bb563e79c1f420d
Lychee <itslychee@protonmail.com>
2023-11-03 17:56:08 -0500

    rpi4 gpu support

--- hosts/fruitpie/configuration.nix
+++ hosts/fruitpie/configuration.nix
@@ -4,11 +4,18 @@
     ../../mixins/security.nix
     (import ../../mixins/openssh.nix { allowedUsers = ["pi"]; })
   ];
+
+
+
+  hardware.raspberry-pi."4" = {
+    fkms-3d.enable = true;
+  };
+
   boot = {
     initrd.kernelModules = [ "vc4" "bcm2835_dma" "i2c_bcm2835" ];
     kernelPackages = pkgs.linuxKernel.kernels.linux_rpi4;
     kernelParams = [ "console=ttyS1,115200n8" ];
-    loader.grub = false;
+    loader.grub.enable = false;
     loader.generic-extlinux-compatible = {
       enable = true;
       configurationLimit = 10;
