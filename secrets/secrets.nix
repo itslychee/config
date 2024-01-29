6869c5985c928961aebd708f713aebd66717dd25
lychee <itslychee@protonmail.com>
2024-01-28 18:21:25 -0600

    hellfire: use new host pubkey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   publicSSHKeys = import ../keys.nix;
   hearthKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHEW9f3t4Ak7o5tVWPdVpLDFxkrrc7ZH9+kn3ZxHJozV";
-  hellfireKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMbJJu96yDOzZrkWoVResruK3t22thNZmTE2/+1m+DdH";
+  hellfireKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICzt2XbvnHZf0gPn68PNMW5jj2YrPfKo1plVh2Dtle+j";
 
   hellfireKeys = [ hellfireKey ] ++ publicSSHKeys;
   hearthKeys = [ hearthKey ] ++ publicSSHKeys;
