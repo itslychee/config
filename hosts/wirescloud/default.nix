a02307861cad6dc0826e4955a0396a63253141f8
lychee <itslychee@protonmail.com>
2024-01-29 18:24:40 -0600

    wirecloud: add hw config

--- hosts/wirescloud/default.nix
+++ hosts/wirescloud/default.nix
@@ -1,4 +1,8 @@
-{ inputs, ...}: {
+{ inputs, lib, modulesPath, ...}: {
+
+
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
 
 
   boot.loader.systemd-boot.enable = true;
