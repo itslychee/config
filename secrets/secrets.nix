871a6b0f71d672a6a1e8c4933fc7d95b5d3690ee
lychee <itslychee@protonmail.com>
2024-01-29 19:43:51 -0600

    secrets fix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   publicSSHKeys = import ../keys.nix;
   hearthKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHEW9f3t4Ak7o5tVWPdVpLDFxkrrc7ZH9+kn3ZxHJozV";
   hellfireKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICzt2XbvnHZf0gPn68PNMW5jj2YrPfKo1plVh2Dtle+j";
-  wirescloudKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIerDKmjJAIelVGXeEsbASrDa1b3srJpfI7XduXO9GUL";
+  wirescloudKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILlm0RhDrXbORKg8dRIl6Z/yct7KJv1FNAouzqA/8BAE";
 
   hellfireKeys = [hellfireKey] ++ publicSSHKeys;
   hearthKeys = [hearthKey] ++ publicSSHKeys;
