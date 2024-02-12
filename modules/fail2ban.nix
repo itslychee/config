b485215c892cfe1cc57cee20a4e5e1d2c260fbf2
lychee <itslychee@protonmail.com>
2024-02-12 06:46:58 -0600

    fail2ban: module init

--- modules/fail2ban.nix
+++ modules/fail2ban.nix
@@ -1,1 +1,24 @@
-{
+{ mylib, lib, config, ...}: let
+  inherit (lib)
+    mkIf
+  ;
+  cfg = config.hey.net.fail2ban;
+in {
+  options.hey.net = {
+    fail2ban = {
+      enable = mylib.mkDefaultOption;
+    };
+  };
+  config = mkIf cfg.enable {
+    services.fail2ban = {
+      enable = true;
+      maxretry = 5;
+      ignoreIP = [
+        "::1"
+        "127.0.0.1"
+      ];
+      bantime = "24h";
+    };
+  };
+
+}
