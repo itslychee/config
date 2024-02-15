b310406661ba8816bff7b69c850286bd1747cae7
lychee <itslychee@protonmail.com>
2024-02-15 17:44:42 -0600

    wirescloud: convert to dendrite

--- hosts/wirescloud/secrets.nix
+++ hosts/wirescloud/secrets.nix
@@ -1,3 +1,5 @@
 {
   age.secrets.lychee-password.file = ../../secrets/lychee-password.age;
+  age.secrets.matrix-serverkey.file = ../../secrets/matrix-serverkey.age;
+  age.secrets.matrix-registration.file = ../../secrets/matrix-registration.age;
 }
--- modules/matrix.nix
+++ modules/matrix.nix
@@ -13,50 +13,55 @@ in {
     matrixHostname = mkStr;
   };
   config = lib.mkIf cfg.enable {
-    services.matrix-conduit = {
-      enable = true;
-      settings.global = {
-        server_name = cfg.serverName;
-	enable_lightning_bolt = false;
+    services = {
+      postgresql = {
+        enable = true;
+        ensureUsers = [{
+          name = "dendrite";
+          ensureDBOwnership = true;
+          ensureClauses.login = true;
+        }];
+        ensureDatabases = [ "dendrite" ];
       };
-    };
-    services.caddy = {
-      enable = true;
-      virtualHosts = {
-          ${cfg.serverName} = let
-            # Psuedo filesystem directory for Caddy to use.
-            matrixDir = pkgs.symlinkJoin {
-              name = "caddy-matrix";
-              paths = [
-                (pkgs.writeTextDir "client" (builtins.toJSON {
-                  "m.homeserver".base_url = "https://${cfg.matrixHostname}"; 
-                }))
-                (pkgs.writeTextDir "server" (builtins.toJSON {
-                  "m.server" = "${cfg.matrixHostname}:443";
-                }))
-              ];
-            };
-          in {
-            extraConfig = ''
-              handle_path /.well-known/matrix/* {
-                root * ${matrixDir}
-                file_server browse
-                header Content-Type application/json
-              }
-            '';
-          };
-          ${cfg.matrixHostname} = {
-	    serverAliases = [
-	    	"${cfg.matrixHostname}:8448"
-	    ];
-            extraConfig = ''
-              reverse_proxy /_matrix* http://[::1]:${toString config.services.matrix-conduit.settings.global.port}
-            '';
-          };
-          
-      };
-    };
-    networking.firewall.allowedTCPPorts = [ 8448 ];
 
+       dendrite = {
+         enable = true;
+         environmentFile = config.age.secrets.matrix-registration.path;
+          settings = {
+            global = {
+              server_name = cfg.serverName;
+              private_key = "/$CREDENTIALS_DIRECTORY/skey";
+              database = {
+                connection_string = "postgresql:///dendrite?host=/run/postgresql";
+              };
+            };
+            client_api = {
+              registration_disabled = true;
+              registration_shared_secret = ''''${REGISTRATION_SHARED_SECRET}'';
+            };
+          };
+          loadCredential = [
+            "skey:${config.age.secrets.matrix-serverkey.path}"
+          ];
+       }; 
+      
+      caddy = {
+        enable = true;
+        virtualHosts = {
+          ${cfg.serverName}.extraConfig = ''
+            header /.well-known/matrix/* Content-Type application/json
+            header /.well-known/matrix/* Access-Control-Allow-Origin *
+            respond /.well-known/matrix/client `{"m.homeserver": {"base_url": "https://${cfg.matrixHostname}"}}`
+            respond /.well-known/matrix/server `{"m.server": "${cfg.matrixHostname}:443"}`
+          '';
+
+          ${cfg.matrixHostname}.extraConfig = ''
+            reverse_proxy /_matrix/* http://[::1]:${toString config.services.dendrite.httpPort}
+            reverse_proxy /_synapse/client/* http://[::1]:${toString config.services.dendrite.httpPort}
+          '';
+        };
+      };
+    };
+    systemd.services.dendrite.after = lib.mkIf config.services.dendrite.enable [ "postgresql.service" ];
   };
 }
