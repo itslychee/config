749f9b307af1d66565d00c5cf81feedf1ba69262
lychee <itslychee@protonmail.com>
2024-02-22 15:17:59 -0600

    lychee: add authorizedKeys if platform equals server or hybrid

--- users/lychee/default.nix
+++ users/lychee/default.nix
@@ -1,14 +1,17 @@
 { config,
  inputs,
+ lib,
  ...
-}: {
+}: let
+  cfg = config.hey.ctx;
+in {
   age.secrets = {
     lychee-password.file = "${inputs.self}/secrets/lychee-password.age";
   };
 
   users.users.lychee = {
     isNormalUser = true;
-    openssh.authorizedKeys.keys = config.hey.keys.users.lychee;
+    openssh.authorizedKeys.keys = lib.mkIf (builtins.elem cfg.platform [ "hybrid" "server"]) config.hey.keys.users.lychee;
     extraGroups = ["wheel"];
     hashedPasswordFile = config.age.secrets.lychee-password.path;
   };
