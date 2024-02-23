d3a9696c988e9639d79c2b70f6537ad7ebbbef62
lychee <itslychee@protonmail.com>
2024-02-22 20:47:58 -0600

    users/lychee: add platform-specific packages

--- users/lychee/default.nix
+++ users/lychee/default.nix
@@ -1,9 +1,11 @@
 { config,
  inputs,
  lib,
+ pkgs,
  ...
 }: let
   cfg = config.hey.ctx;
+  inherit (lib) mkIf mkMerge;
 in {
   age.secrets = {
     lychee-password.file = "${inputs.self}/secrets/lychee-password.age";
@@ -11,12 +13,22 @@ in {
 
   users.users.lychee = {
     isNormalUser = true;
-    openssh.authorizedKeys.keys = lib.mkIf (builtins.elem cfg.platform [ "hybrid" "server"]) config.hey.keys.users.lychee;
+    openssh.authorizedKeys.keys = mkIf (builtins.elem cfg.platform [ "hybrid" "server"]) config.hey.keys.users.lychee;
     extraGroups = ["wheel"];
     hashedPasswordFile = config.age.secrets.lychee-password.path;
   };
   hey.users.lychee = {
     wrappers.enable = true;
+    packages = mkMerge [
+      # Desktop only apps
+      (mkIf (builtins.elem cfg.platform ["hybrid" "client"]) builtins.attrValues {
+        inherit (pkgs) firefox;
+      })
+      # Server only
+      (mkIf (builtins.elem cfg.platform ["hybrid" "server"]) builtins.attrValues {
+      })
+      
+   ];
   };
 
 }
