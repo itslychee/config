1a8d8ff6a297a0d8ddd3bacce3e3433e4d10def7
itslychee <itslychee@protonmail.com>
2024-02-24 20:04:07 -0600

    nix: remove builders, disable channels, set nixpkgs to flake registry nixpkgs

--- modules/nix.nix
+++ modules/nix.nix
@@ -11,35 +11,17 @@
 in {
   options.hey.nix = {
     enable = mylib.mkDefaultOption;
-    emulation = mylib.mkDefaultOption;
   };
   config = mkIf nix.enable {
     nixpkgs.config.allowUnfree = true;
     nix = {
+      nixPath = ["nixpkgs=flake:nixpkgs"];
       sshServe = {
         protocol = "ssh-ng";
         keys = config.hey.keys.users.lychee;
         enable = true;
       };
-      distributedBuilds = true;
-      buildMachines = [
-        {
-          hostName = "hearth";
-          systems = ["x86_64-linux" "aarch64-linux"];
-          protocol = "ssh-ng";
-          speedFactor = 10;
-          maxJobs = 10;
-          sshUser = "nix-builder";
-        }
-        {
-          hostName = "wirescloud";
-          systems = ["x86_64-linux" "aarch64-linux"];
-          protocol = "ssh-ng";
-          speedFactor = 20;
-          maxJobs = 20;
-          sshUser = "nix-builder";
-        }
-      ];
+      channel.enable = false;
       settings = {
         trusted-users = ["@wheel" "root"];
         builders-use-substitutes = true;
@@ -53,12 +35,6 @@ in {
           "cgroups"
         ];
       };
-    };
-
-    users.users.nix-builder = {
-      shell = pkgs.bash;
-      isNormalUser = true;
-      description = "remote builder";
     };
   };
 }
