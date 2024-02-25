5deca99e3fda08298d1656fa53df21297f506ed6
itslychee <itslychee@protonmail.com>
2024-02-24 19:57:52 -0600

    hw-config: remove stupid comments that i do not care about

--- hosts/hearth/hardware-configuration.nix
+++ hosts/hearth/hardware-configuration.nix
@@ -30,13 +30,5 @@
 
   swapDevices = [{device = "/dev/disk/by-uuid/d5493993-21a0-40e2-a63f-32d6e3586129";}];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = false;
-  # networking.interfaces.enp10s0.useDHCP = lib.mkDefault true;
-  networking.interfaces.wlp9s0.useDHCP = lib.mkDefault true;
-
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
