83ee3c2a12d4345bdc4446bd0808a022750d4ea9
itslychee <itslychee@protonmail.com>
2024-03-04 00:30:56 -0600

    keybindings: smol refactoring

--- users/lychee/default.nix
+++ users/lychee/default.nix
@@ -5,7 +5,7 @@
   pkgs,
   ...
 }: let
-  inherit (lib) getExe;
+  inherit (lib) getExe getExe';
   cfg = config.hey.users;
   inherit (lib) mkIf;
 in {
@@ -35,7 +35,7 @@ in {
       '';
       keybindings = let
         _ = getExe;
-        pamixer = getExe pkgs.pamixer;
+        __ = getExe';
         player = "${_ pkgs.playerctl} --player='spotify,mpd,%any'";
         modifier = "Mod4";
         wayshot = "${_ pkgs.wayshot} -s \"$(${_ pkgs.slurp})\" --stdout";
@@ -44,10 +44,10 @@ in {
         Print = "exec ${wayshot} | wl-copy";
         "Shift+Print" = "exec ${wayshot} | ${_ pkgs.swappy} -f - -o - | wl-copy";
         "Control+Print" = "exec ${_ wl-screenrec} -g \"`${_ pkgs.slurp}`\" -f ~/media/screenshots/\"`date +'%F_%H.%H_%M_%N'`\".mp4";
-        "Control+Delete" = "exec ${lib.getExe' pkgs.procps "pkill"} wl-screenrec";
-        XF86AudioRaiseVolume = "exec ${pamixer} -i 2";
-        XF86AudioLowerVolume = "exec ${pamixer} -d 2";
-        XF86AudioMute = "exec ${pamixer} -t";
+        "Control+Delete" = "exec ${__ pkgs.procps "pkill"} wl-screenrec";
+        XF86AudioRaiseVolume = "exec ${_ pkgs.pamixer} -i 2";
+        XF86AudioLowerVolume = "exec ${_ pkgs.pamixer} -d 2";
+        XF86AudioMute = "exec ${_ pkgs.pamixer} -t";
         XF86AudioStop = "exec ${player} stop";
         XF86AudioPrev = "exec ${player} previous";
         XF86AudioPlay = "exec ${player} play-pause";
@@ -67,8 +67,6 @@ in {
         "${modifier}+c" = "workspace 3";
         "${modifier}+v" = "workspace 4";
         "${modifier}+b" = "workspace 5";
-        "${modifier}+bracketright" = "workspace next";
-        "${modifier}+bracketleft" = "workspace prev";
         "${modifier}+w" = "kill";
         "${modifier}+s" = "sticky toggle";
         "${modifier}+d" = "floating toggle";
