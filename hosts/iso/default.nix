e4c827103942b6c054f3b66356ed30d821c0e3c2
itslychee <itslychee@protonmail.com>
2024-03-07 10:42:39 -0600

    iso: remove docs option

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -1,12 +1,12 @@
-{
-  inputs,
-  lib,
-  modulesPath,
-  ...
-}: {
-  imports = [
-    (modulesPath + "/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix")
-  ];
-  # Documentation is not desired on ephemeral-like systems
-  documentation.enable = lib.mkForce false;
+{ lib, modulesPath, mylib, config, ... }:
+let
+  inherit (lib) mkForce;
+in {
+  imports = [ (modulesPath +"/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix") ];
+  networking.networkmanager.enable = true;
+  networking.wireless.enable = mkForce false;
+  hey.services.openssh.enable = true;
+  users.users = {
+     root.openssh.authorizedKeys.keys = mylib.keys.privileged config.hey.keys.users.lychee;
+  };
 }
