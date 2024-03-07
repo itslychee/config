9478a311a9f72484112b354eb7047b0e775e96d3
itslychee <itslychee@protonmail.com>
2024-03-07 10:35:25 -0600

    agenix: don't override age binary

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -3,18 +3,9 @@
   inputs,
   lib,
   ...
-}: let
-  ageBin = lib.getExe pkgs.rage;
-in {
+}: {
   imports = [inputs.agenix.nixosModules.default];
-
-  # I want my system to reflect my configuration more
-  users.mutableUsers = false;
-
   environment.systemPackages = [
-    (inputs.agenix.packages.${pkgs.system}.default.override {
-      inherit ageBin;
-    })
+      inputs.agenix.packages.${pkgs.system}.default
   ];
-  age.ageBin = ageBin;
 }
