f67ed55e58748359685ae00d5e2f294ff2c66263
itslychee <itslychee@protonmail.com>
2024-03-10 06:44:33 -0500

    development: move git and starship to modules/development.nix

--- modules/default.nix
+++ modules/default.nix
@@ -21,7 +21,6 @@
   # Global options
   time.timeZone = "US/Central";
 
-  environment.defaultPackages = [pkgs.git];
   environment.etc."xdg/user-dirs.default".text = ''
     XDG_DESKTOP_DIR="$HOME/desktop"
     XDG_DOCUMENTS_DIR="$HOME/documents"
@@ -33,37 +32,4 @@
     XDG_VIDEOS_DIR="$HOME/media/videos"
   '';
 
-  programs.direnv = {
-    enable = true;
-    silent = true;
-    nix-direnv.enable = true;
-  };
-
-  programs.starship = {
-    enable = true;
-    settings = {
-      hostname.format = "in [$ssh_symbol$hostname]($style)";
-      username = {
-        style_user = "bold cyan";
-        format = "[$user]($style)";
-      };
-      nix_shell.format = "$symbol[nixshell]($style)";
-      python.format = "[\${symbol}\${pyenv_prefix}(\${version} )(($virtualenv) )]($style)";
-      rust.format = "[$symbol($version )]($style)";
-      git_branch = {
-        symbol = "  ";
-        format = "[$symbol$branch(:$remote_branch)]($style)";
-      };
-      golang.format = "go($version)";
-      format = ''
-        [\[$directory$git_branch$git_state$git_commit\]](bold green)
-        [❤️ $username$hostname](bold #ff9ad2)$character
-      '';
-      right_format = "$rust$golang$python$nix_shell";
-      character = {
-        success_symbol = "➜";
-        error_symbol = "➜";
-      };
-    };
-  };
 }
