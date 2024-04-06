91b288a08991f7ab0dc813b4e4102ac1f9e3c256
itslychee <itslychee@protonmail.com>
2024-04-05 20:35:27 -0500

    add home-manager input

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     disko.url = "github:nix-community/disko";
     deploy.url = "github:serokell/deploy-rs";
     wiresbot.url = "github:itslychee/wires-bot";
+    home-manager.url = "github:nix-community/home-manager";
   };
   outputs = {
     self,
