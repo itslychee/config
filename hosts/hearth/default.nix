d686fb4995069f5ad50ee344b064e9594262eb74
itslychee <itslychee@protonmail.com>
2024-04-09 23:10:19 -0500

    hearth: enable steam

--- hosts/hearth/default.nix
+++ hosts/hearth/default.nix
@@ -64,6 +64,12 @@
   environment.sessionVariables.NIX_INDEX_DATABASE = "/var/lib/nix-index-db";
   programs.nix-index.enable = true;
 
+
+  programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+  };
+
   # do not touch ever! #
   system.stateVersion = "24.05";
 }
