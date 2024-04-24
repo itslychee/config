c5a1a6c7e038f17e4b6e2e56a99f5d53a20d731f
itslychee <itslychee@protonmail.com>
2024-04-23 20:36:16 -0500

    add garnix as a trusted substituter

--- modules/nix.nix
+++ modules/nix.nix
@@ -22,6 +22,7 @@ in {
         inputs;
       settings = {
         substituters = ["https://cache.garnix.io"];
+        trusted-substituters = ["https://cache.garnix.io"];
         trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
         trusted-users = ["@wheel" "root"];
         builders-use-substitutes = true;
