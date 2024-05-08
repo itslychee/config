ea09f349a3555787938004f0081babe6c177baf0
itslychee <itslychee@protonmail.com>
2024-05-07 20:46:34 -0500

    iso: fix mkIf usage for usePasswdFile

--- modules/users.nix
+++ modules/users.nix
@@ -3,21 +3,27 @@
   lib,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mkMerge;
   cfg = config.hey.users.lychee;
 in {
-  age.secrets.lychee-password.file = mkIf cfg.usePasswdFile ../secrets/lychee-password.age;
-  # should be obvious why this is global
-  hey.users.lychee = {
-    enable = true;
-    usePasswdFile = true;
-    sshKeys = config.hey.keys.users.lychee.ssh;
-    passwordFile = mkIf cfg.usePasswdFile config.age.secrets.lychee-password.path;
-    groups = [
-      "wheel"
-      "audio"
-      "video" # needed for light
-      "networkmanager"
-    ];
-  };
+  config = mkMerge [
+    (mkIf cfg.usePasswdFile {
+      age.secrets.lychee-password.file = mkIf cfg.usePasswdFile ../secrets/lychee-password.age;
+    })
+    {
+      # should be obvious why this is global
+      hey.users.lychee = {
+        enable = true;
+        usePasswdFile = true;
+        sshKeys = config.hey.keys.users.lychee.ssh;
+        passwordFile = mkIf cfg.usePasswdFile config.age.secrets.lychee-password.path;
+        groups = [
+          "wheel"
+          "audio"
+          "video" # needed for light
+          "networkmanager"
+        ];
+      };
+    }
+  ];
 }
