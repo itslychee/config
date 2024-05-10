8c07ef26629a12bf1b2c328ddcf0187b230062b1
itslychee <itslychee@protonmail.com>
2024-05-10 11:14:04 -0500

    nix: remove garnix from substituters

--- modules/nix.nix
+++ modules/nix.nix
@@ -15,9 +15,6 @@ in {
       })
       inputs;
     settings = {
-      substituters = ["https://cache.garnix.io"];
-      trusted-substituters = ["https://cache.garnix.io"];
-      trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
       trusted-users = ["@wheel" "root"];
       builders-use-substitutes = true;
       use-xdg-base-directories = true;
