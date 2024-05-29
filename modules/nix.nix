819a52c7daa5ef04d3335407f608b3f23985eaf7
itslychee <itslychee@protonmail.com>
2024-05-29 01:29:17 -0500

    nix: ignore system registry

--- modules/nix.nix
+++ modules/nix.nix
@@ -15,6 +15,7 @@ in {
       })
       inputs;
     settings = {
+      flake-registry = "";
       substituters = ["https://cache.garnix.io"];
       trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
       trusted-users = ["@wheel" "root"];
