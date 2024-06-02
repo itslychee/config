79746903c51b8831afa1a91c9bcc9f7dbe113492
itslychee <itslychee@protonmail.com>
2024-06-02 15:23:47 -0500

    iso: add lvm kernel modules

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -8,14 +8,26 @@
 in {
   imports = [(modulesPath + "/installer/cd-dvd/installation-cd-minimal-new-kernel.nix")];
 
+  # Support LVM
+  boot.kernelModules = [
+    "dm-snapshot"
+    "dm-mod"
+    "dm-cache"
+    "dm-cache-default"
+  ];
+
   isoImage.isoBaseName = mkForce "configuration";
   networking.networkmanager.enable = true;
   networking.wireless.enable = mkForce false;
 
   services.kmscon.autologinUser = "lychee";
+
+  # Disable defaults that don't make sense here
   hey.users.lychee.usePasswdFile = mkForce false;
   services.tailscale.enable = mkForce false;
   hey.net.fail2ban = mkForce false;
+
+  # Caps
   hey.caps = {
     rootLogin = true;
     headless = true;
