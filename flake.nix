5513d2942ac8721b10f3c9a24b0fa5a414a2a604
itslychee <itslychee@protonmail.com>
2024-06-08 10:30:39 -0500

    home-manager: track release 24.05

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     mpdrp.inputs.nixpkgs.follows = "nixpkgs";
 
     # home manager
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = {
