cba96fa77b2d7c7db1f99e2e9ef1353e0f2437fb
itslychee <itslychee@protonmail.com>
2024-06-09 05:51:48 -0500

    neovim: use F for git_files instead of g

--- nvim/lua/fruit/telescope.lua
+++ nvim/lua/fruit/telescope.lua
@@ -1,6 +1,6 @@
 local ts = require "telescope.builtin"
 local k = vim.keymap.set
 k("n", "<leader>f", ts.find_files)
+k("n", "<leader>F", ts.git_files)
 k("n", "<leader>G", ts.live_grep)
-k("n", "<leader>g", ts.git_files)
 k("n", "<leader>b", ts.buffers)
