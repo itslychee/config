e641ea75126f9fb79933d547450dbe14c2c858bc
itslychee <itslychee@protonmail.com>
2024-06-14 06:47:11 -0500

    modules/users.nix: refactoring

--- modules/users.nix
+++ modules/users.nix
@@ -4,54 +4,52 @@
   lib,
   ...
 }: let
-  inherit (lib) mkIf mkMerge flatten optionals;
-  cfg = config.hey.users.lychee;
+  inherit
+    (lib)
+    flatten
+    optionals
+    ;
 in {
-  config = mkMerge [
-    (mkIf cfg.usePasswdFile {
-      age.secrets.lychee-password.file = ../secrets/lychee-password.age;
-    })
-    {
-      # should be obvious why this is global
-      hey.users.lychee = {
-        enable = true;
-        usePasswdFile = true;
-        sshKeys = config.hey.keys.users.lychee.ssh;
-        passwordFile = mkIf cfg.usePasswdFile config.age.secrets.lychee-password.path;
-        groups = [
-          "wheel"
-          "audio"
-          "libvirt"
-          "video" # needed for light
-          "networkmanager"
-          "wireshark"
-        ];
-        packages = flatten [
-          (optionals config.hey.caps.graphical [
-            (pkgs.discord-canary.override {withVencord = true;})
-            pkgs.anki
-            pkgs.qbittorrent
-            pkgs.firefox
-          ])
-        ];
-      };
+  # should be obvious why this is global
+  hey.users.lychee = {
+    enable = true;
+    usePasswdFile = true;
+    sshKeys = config.hey.keys.users.lychee.ssh;
+    passwordFile =
+      (pkgs.writeText
+        "hash" "$2b$05$XpUQp7q4DFZT.gbHyzcwqe9Vl57WtX9stTrbH04ilX9EMVthaiF1O")
+      .outPath;
+    groups = [
+      "wheel"
+      "audio"
+      "libvirt"
+      "video" # needed for light
+      "networkmanager"
+      "wireshark"
+    ];
+    packages = flatten [
+      (optionals config.hey.caps.graphical [
+        (pkgs.discord-canary.override {withVencord = true;})
+        pkgs.anki
+        pkgs.qbittorrent
+        pkgs.firefox
+      ])
+    ];
+  };
 
-      environment.systemPackages = builtins.attrValues {
-        inherit
-          (pkgs)
-          htop
-          ripgrep
-          jq
-          nmap
-          nixpkgs-review
-          ;
-      };
+  environment.systemPackages = builtins.attrValues {
+    inherit
+      (pkgs)
+      htop
+      ripgrep
+      jq
+      nmap
+      nixpkgs-review
+      ;
+  };
 
-      programs.wireshark = {
-        package = pkgs.wireshark;
-        enable = config.hey.caps.graphical;
-      };
-
-    }
-  ];
+  programs.wireshark = {
+    package = pkgs.wireshark;
+    enable = config.hey.caps.graphical;
+  };
 }
