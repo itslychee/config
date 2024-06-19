20fce57d143b6751028ea6de2165e65b471562d2
itslychee <itslychee@protonmail.com>
2024-06-18 21:22:24 -0500

    big-floppa: emulate aarch64 for builds

--- hosts/big-floppa/default.nix
+++ hosts/big-floppa/default.nix
@@ -8,6 +8,7 @@
 in {
   boot.kernelParams = ["intel_iommu=on"];
   boot.loader.systemd-boot.enable = true;
+  boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
   hey = {
     hostKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICAKg9ZgbTR5ftw+nrm+Ch7Xl4LBs4z9M+e45/K0pG4u";
