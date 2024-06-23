d4fbf84303ca0933d5df017f60832f055f24518e
itslychee <itslychee@protonmail.com>
2024-06-22 21:07:03 -0500

    pathway: fix broken config

--- hosts/pathway/default.nix
+++ hosts/pathway/default.nix
@@ -15,33 +15,9 @@
   hey = {
     hostKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICE5cYKkvfT55xxhmLirU6K+JAHaZNd0xCsXPYrTuAEu";
     caps.headless = true;
-    caps.graphical = true;
     net.home = true;
-    users.lychee.enable = lib.mkForce false;
   };
 
-  # doesnt work for reasons idk (black screen, see cursor, HDMI-CEC seems to not work?)
-  #
-  # services.kmscon.enable = lib.mkForce false;
-  # services.greetd.enable = lib.mkForce false;
-  # services.displayManager.sddm = {
-  #   enable = true;
-  #   wayland.enable = true;
-  #   settings = {
-  #     Autologin = {
-  #       User = "viewer";
-  #       Session = "plasma-bigscreen-x11.desktop";
-  #       Relogin = true;
-  #     };
-  #   };
-  #   theme = "chili";
-  # };
-  # environment.systemPackages = [pkgs.sddm-chili-theme];
-  #
-  # services.xserver.enable = true;
-  # services.xserver.desktopManager.plasma5 = {
-  #   bigscreen.enable = true;
-  # };
   services.logind.lidSwitchExternalPower = "ignore";
   system.stateVersion = "23.11"; # Did you read the comment?
 }
