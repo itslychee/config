3408de58a35a1cf7fae5aef3ebef8c1807d41044
itslychee <itslychee@protonmail.com>
2024-06-22 19:53:24 -0500

    networking: cleanup

--- modules/networking.nix
+++ modules/networking.nix
@@ -43,54 +43,49 @@ in {
       home = mkEnableOption "Home networking";
     };
   };
-  config = mkMerge [
-    (mkIf cfg.home {
-      # https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1658731959
-      systemd.services.NetworkManager-wait-online.serviceConfig = {
-        ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
+  config = {
+    services = {
+      tailscale = {
+        enable = true;
+        useRoutingFeatures = "both";
+        openFirewall = true;
       };
-      networking.networkmanager.unmanaged = [config.services.tailscale.interfaceName];
-    })
-    {
-      services = {
-        tailscale = {
-          enable = true;
-          useRoutingFeatures = "both";
-          openFirewall = true;
-        };
-        fail2ban = {
-          enable = cfg.fail2ban;
-          maxretry = 5;
-          ignoreIP = [
-            "::1"
-            "127.0.0.1"
-            "100.0.0.0/8"
-          ];
-          bantime = "24h";
-        };
-        # OpenSSH server
-        openssh = {
-          enable = cfg.openssh;
-          authorizedKeysInHomedir = false;
-          settings = {
-            PasswordAuthentication = false;
-          };
+      fail2ban = {
+        enable = cfg.fail2ban;
+        maxretry = 5;
+        ignoreIP = [
+          "::1"
+          "127.0.0.1"
+          "100.0.0.0/8"
+        ];
+        bantime = "24h";
+      };
+      # OpenSSH server
+      openssh = {
+        enable = cfg.openssh;
+        authorizedKeysInHomedir = false;
+        settings = {
+          PasswordAuthentication = false;
         };
       };
+    };
 
-      # Convenience!
-      networking.firewall.allowedTCPPorts = mkIf config.services.caddy.enable [80 443];
-      # Convenience!
-      networking.networkmanager.enable = true;
-      programs.ssh = {
-        startAgent = true;
-        agentTimeout = "30m";
-        extraConfig = ''
-          Host big-floppa
-            User student
-        '';
-      };
-      systemd.network.wait-online.ignoredInterfaces = [config.services.tailscale.interfaceName];
-    }
-  ];
+    # Convenience!
+    networking.firewall.allowedTCPPorts = mkIf config.services.caddy.enable [80 443];
+    # https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1658731959
+    systemd.services.NetworkManager-wait-online.serviceConfig = {
+      ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
+    };
+    networking.networkmanager.unmanaged = [config.services.tailscale.interfaceName];
+    networking.networkmanager.enable = true;
+    programs.ssh = {
+      startAgent = true;
+      agentTimeout = "30m";
+      extraConfig = ''
+        Host big-floppa
+          User student
+      '';
+    };
+    systemd.network.wait-online.ignoredInterfaces = [config.services.tailscale.interfaceName];
+  };
 }
