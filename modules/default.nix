3d31434c951dd9fc733f72f442b03bbf72bc068f
itslychee <itslychee@protonmail.com>
2024-06-24 09:57:29 -0500

    xdg-dirs: remove

--- modules/default.nix
+++ modules/default.nix
@@ -32,15 +32,5 @@ in {
     boot.blacklistedKernelModules = [
       "uvcvideo"
     ];
-    environment.etc."xdg/user-dirs.defaults".text = ''
-      XDG_DESKTOP_DIR="$HOME/desktop"
-      XDG_DOCUMENTS_DIR="$HOME/documents"
-      XDG_DOWNLOAD_DIR="$HOME/downloads"
-      XDG_MUSIC_DIR="$HOME/media/music"
-      XDG_PICTURES_DIR="$HOME/media/pictures"
-      XDG_PUBLICSHARE_DIR="$HOME/public"
-      XDG_TEMPLATES_DIR="$HOME/media/templates"
-      XDG_VIDEOS_DIR="$HOME/media/videos"
-    '';
   };
 }
