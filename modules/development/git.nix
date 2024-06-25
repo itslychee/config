ceab1c2d5b490bbdb3ef411c258a138ad6744171
itslychee <itslychee@protonmail.com>
2024-06-25 11:46:43 -0500

    git: add l alias for log

---
+++ modules/development/git.nix
@@ -0,0 +1,6 @@
+{
+  environment.etc."gitconfig".text = ''
+    [alias]
+    l = "log --graph --all --oneline --decorate --pretty='format:%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+  '';
+}
