39c49ceda61eacca12b89a5e5508e7c69252cd85
itslychee <itslychee@protonmail.com>
2024-06-27 19:13:45 -0500

    blocky: disable for right now and trim the ad lists

--- hosts/hellfire/blocky.nix
+++ hosts/hellfire/blocky.nix
@@ -1,10 +1,11 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   services.blocky = {
-    enable = true;
+    enable = false;
     settings = {
       caching = {
         minTime = "20m";
@@ -12,38 +13,23 @@
       };
       upstreams.groups = {
         default = [
-          # CF
           "1.1.1.1"
           "1.0.0.1"
           "8.8.8.8"
-          # ControlD
-          "76.76.2.2"
-          "76.76.10.2"
+          "2606:4700:4700::1111"
+          "2606:4700:4700::1001"
         ];
       };
-      bootstrapDns = [
-        "tcp+udp:1.1.1.1"
-      ];
       customDNS.customTTL = "24h";
       blocking.blackLists.ads = [
-        "https://adaway.org/hosts.txt"
-        "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
-        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"
-        "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt"
-        "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts"
-        "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
-        "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
-        "https://v.firebog.net/hosts/AdguardDNS.txt"
-        "https://v.firebog.net/hosts/Admiral.txt"
-        "https://v.firebog.net/hosts/Easylist.txt"
-        "https://v.firebog.net/hosts/Easyprivacy.txt"
-        "https://v.firebog.net/hosts/Prigent-Ads.txt"
+        "${pkgs.stevenblack-blocklist}/hosts"
       ];
       blocking.clientGroupsBlock.default = ["ads"];
     };
   };
 
   networking.firewall = lib.mkIf config.services.blocky.enable {
+    # DNS port
     allowedTCPPorts = [53];
     allowedUDPPorts = [53];
   };
