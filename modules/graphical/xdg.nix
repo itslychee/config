a2305b098b49efe30c398190b7bd279877107db4
itslychee <itslychee@protonmail.com>
2024-06-29 02:36:52 -0500

    xdg: fix xdg portals

--- modules/graphical/xdg.nix
+++ modules/graphical/xdg.nix
@@ -1,4 +1,9 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   environment.sessionVariables = {
     XDG_DESKTOP_DIR = "$HOME/desktop";
     XDG_DOCUMENTS_DIR = "$HOME/documents";
@@ -8,5 +13,11 @@
     XDG_PUBLICSHARE_DIR = "$HOME/media/public";
     XDG_TEMPLATES_DIR = "$HOME/media/templates";
     XDG_VIDEOS_DIR = "$HOME/media/videos";
+  };
+
+  xdg.portal = lib.mkIf config.hey.caps.graphical {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-kde pkgs.xdg-desktop-portal-gtk];
   };
 }
