96d71b58053c300732dbb15b70c510285c41f0b8
itslychee <itslychee@protonmail.com>
2024-07-03 21:28:20 -0500

    neovim: better grep

--- nvim/lua/fruit/pickin.lua
+++ nvim/lua/fruit/pickin.lua
@@ -15,6 +15,29 @@ k("n", "<leader>F", function()
     source = { items = vim.fn.readdir "." },
   }
 end, { desc = "[pick] Find files in current directory" })
-k("n", "<leader>g", pick.builtin.grep, {
-  desc = "[pick] Grep",
-})
+
+-- https://github.com/mrshmllow/nvim-candy/blob/26f90fd23be1d3ce85340195ed28307f8cc55918/candy/lua/marshmallow/remap.lua#L40-L61
+-- <3333
+
+k("n", "<leader>g", function()
+  local cope = function()
+    local items = require("mini.pick").get_picker_items()
+
+    vim.fn.setqflist(vim.tbl_map(function(value)
+      local split = vim.split(value, ":")
+      local text = table.concat(split, "", 4)
+
+      return {
+        filename = split[1],
+        lnum = split[2],
+        col = split[3],
+        text = vim.trim(text),
+      }
+    end, items))
+
+    vim.cmd.cope()
+    return true
+  end
+  local buffer_mappings = { wipeout = { char = "<C-q>", func = cope } }
+  require("mini.pick").builtin.grep_live({}, { mappings = buffer_mappings })
+end, { desc = "Grep (root dir)" })
