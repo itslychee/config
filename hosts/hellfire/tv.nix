5365b94dcb45447d2ca24338f8e344a2de858edc
itslychee <itslychee@protonmail.com>
2024-07-06 23:50:13 -0500

    hellfire: initial kodi support

--- hosts/hellfire/tv.nix
+++ hosts/hellfire/tv.nix
@@ -5,19 +5,28 @@
 }: {
   hey.caps.graphical = true;
   programs.wireshark.enable = lib.mkForce false;
+  hey.programs.neovim.enable = false;
+
+  # Disable lychee, she does not exist here!!
+  hey.users.lychee.enable = lib.mkForce false;
+  hey.users.viewer = {
+    enable = true;
+    groups = ["video" "audio" "input"];
+  };
+
   services.xserver = {
     enable = true;
-    desktopManager.kodi = {
-      package = pkgs.kodi-wayland;
+    displayManager.lightdm = {
       enable = true;
+      extraSeatDefaults = ''
+        autologin-user=viewer
+        autologin-user-timeout=0
+        autologin-session=kodi
+      '';
     };
-
-    displayManager = {
-      defaultSession = "kodi";
-      lightdm = {
-        enable = true;
-        autoLogin.timeout = 1;
-      };
+    desktopManager.kodi = {
+      enable = true;
+      package = pkgs.kodi-gbm;
     };
   };
 }
