7ec572cdc2827380421665325665d52d432c4b08
itslychee <itslychee@protonmail.com>
2024-08-05 19:56:53 -0500

    kaycloud: remove syncthing for rn

--- hosts/kaycloud/default.nix
+++ hosts/kaycloud/default.nix
@@ -2,13 +2,6 @@
   hey.caps.headless = true;
   networking.networkmanager.enable = false;
 
-  services.syncthing = {
-    enable = true;
-    settings.options.listenAddress = [
-      "tcp://${config.networking.hostName}"
-    ];
-  };
-
   networking.firewall.interfaces.${config.services.tailscale.interfaceName} = {
     allowedTCPPorts = [22000 8384];
     allowedUDPPorts = [21027 22000];
