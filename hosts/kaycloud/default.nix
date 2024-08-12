cc7721e158c9de6069584f8326f02e283d084c05
itslychee <itslychee@protonmail.com>
2024-08-12 10:09:15 -0500

    kaycloud: set up IPv6 address

--- hosts/kaycloud/default.nix
+++ hosts/kaycloud/default.nix
@@ -1,9 +1,26 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  nodes,
+  ...
+}: {
   hey.caps.headless = true;
   networking.networkmanager.enable = false;
 
   networking.firewall.interfaces.${config.services.tailscale.interfaceName} = {
     allowedTCPPorts = [22000 8384];
     allowedUDPPorts = [21027 22000];
+  };
+
+  # IPv6 public IP
+  systemd.network = {
+    enable = true;
+    networks.ipv6 = {
+      matchConfig.Name = "enp1s0";
+      networkConfig = {
+        Address = "2a01:4ff:f0:cb64::1/64";
+        Gateway = "fe80::1";
+      };
+    };
   };
 }
