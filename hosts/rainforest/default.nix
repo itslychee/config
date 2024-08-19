4154a9c063288ad180a6bc9548c27d2e71088ad9
itslychee <itslychee@protonmail.com>
2024-08-19 14:25:09 -0500

    rainforest: remove nextcloud and libvirtd

--- hosts/rainforest/default.nix
+++ hosts/rainforest/default.nix
@@ -10,31 +10,7 @@
   hey = {
     hostKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICAKg9ZgbTR5ftw+nrm+Ch7Xl4LBs4z9M+e45/K0pG4u";
     caps.headless = true;
-    users.lychee.groups = ["nextcloud"];
   };
 
-  services.nginx.enable = true;
-  services.nextcloud = {
-    enable = true;
-    hostName = "cloud.rain.forest";
-    configureRedis = true;
-    package = pkgs.nextcloud29;
-    database.createLocally = true;
-    autoUpdateApps.enable = true;
-    caching.redis = true;
-    config = {
-      dbtype = "pgsql";
-      adminuser = "administrator";
-      adminpassFile = "/var/lib/nextcloud/password";
-    };
-    settings.trusted_domains = [
-      config.networking.hostName
-      config.services.nextcloud.hostName
-    ];
-  };
-
-  virtualisation.libvirtd.enable = true;
-
-  networking.firewall.allowedTCPPorts = [80 443 25565];
   system.stateVersion = "23.11";
 }
