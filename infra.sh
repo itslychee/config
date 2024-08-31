2ccaea4c1ef89e526fb523cd12dcbf2eac1b7d41
itslychee <itslychee@protonmail.com>
2024-08-30 22:49:40 -0500

    infra.sh: whatever

---
+++ infra.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+nodes=$(nix eval --apply 'with builtins; l: (filter (x: !elem x [ "defaults" "meta" "network" ])) (builtins.attrNames l)' --no-warn-dirty  --json .#colmena  | jq -r ".[]")
+RESET="\e[0m"
+GREEN="\e[32m"
+MAGNETA="\e[35m"
+LIGHTRED="\e[91m"
+
+
+execute_command() {
+    set +e
+    ssh -o ConnectTimeout=5 "$1" -C "$2"
+    set -e
+}
+
+for x in $nodes; do
+    echo -e "$GREEN$x$RESET: running $MAGNETA$@$RESET"
+    execute_command $x "$@"
+done
+
