28b30ff7f8e3c7a4809b4e0b1b9f7d36ba77c5f8
itslychee <itslychee@protonmail.com>
2024-09-19 13:09:15 -0500

    server: dont disable man

--- modules/roles/server/default.nix
+++ modules/roles/server/default.nix
@@ -8,13 +8,6 @@ in {
 
   environment.sessionVariables.BROWSER = "echo ";
 
-  # Notice this also disables --help for some commands such es nixos-rebuild
-  documentation = {
-    enable = mkDefault false;
-    info.enable = mkDefault false;
-    man.enable = mkDefault false;
-    nixos.enable = mkDefault false;
-  };
   boot.kernel.sysctl = {
     "net.core.default_qdisc" = "fq";
     "net.ipv4.tcp_congestion_control" = "bbr";
