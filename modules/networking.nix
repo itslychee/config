542600d2d42ae4c18c0aff3503bebc95993584a1
itslychee <itslychee@protonmail.com>
2024-09-23 10:17:01 -0500

    NetworkManager-wait-online: ugh

---
+++ modules/networking.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  # https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1658731959
+  systemd.services.NetworkManager-wait-online = {
+    serviceConfig = {
+      ExecStart = [
+        ""
+        "${pkgs.networkmanager}/bin/nm-online -q"
+      ];
+    };
+  };
+}
