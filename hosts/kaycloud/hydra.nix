634d18d7c6aa30826c192516e5be5eee5849832f
itslychee <itslychee@protonmail.com>
2024-09-24 09:23:16 -0500

    add auto attic uploader

--- hosts/kaycloud/hydra.nix
+++ hosts/kaycloud/hydra.nix
@@ -1,4 +1,9 @@
-{ config, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   services.hydra = {
     enable = true;
@@ -12,6 +17,30 @@
     virtualHosts."hydra.wires.cafe".extraConfig = ''
       reverse_proxy http://[::1]:${toString config.services.hydra.port}
     '';
+  };
+
+  deployment.keys.attic-secret = {
+    destDir = "/var/lib/secrets/hydra";
+    keyCommand = [
+      "gpg"
+      "--decrypt"
+      "${../../secrets/hydra-attic-key.gpg}"
+    ];
+  };
+
+  systemd.services.hydra-watcher = {
+    wantedBy = [
+      "multi-user.target"
+    ];
+    requires = [ "atticd.service" ];
+    path = [ pkgs.attic-client ];
+    script = ''
+      attic login wires https://cache.wires.cafe $TOKEN
+      attic watch-store lychee-config
+    '';
+    serviceConfig = {
+      EnvironmentFile = config.deployment.keys.attic-secret.path;
+    };
   };
 
   hey.remote.use = true;
