2ed17f65f629c4d49edf023908a334eedbb592a5
itslychee <itslychee@protonmail.com>
2024-11-02 00:29:36 -0500

    rainforest-node-1,2: hope removing these mkForce for hostname doesn't break anything

--- hosts/rainforest-node-1/default.nix
+++ hosts/rainforest-node-1/default.nix
@@ -1,11 +1,11 @@
 { lib, config, ... }:
 {
-  networking.hostName = lib.mkForce "rainforest-node-2";
   boot.loader.systemd-boot.enable = true;
   hey.hostKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHiCax0o/1zd+Ry7impZRDbOn4F3ife/A1HhS0EYh1KH root@rainforest-node-2"
   ];
   system.stateVersion = "24.05";
+  hey.mine = true;
 
   hey.remote.builder = {
     enable = true;
