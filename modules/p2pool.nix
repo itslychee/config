75bc0ec804ae10180ef5a875dd3de0f68fbbcd43
itslychee <itslychee@protonmail.com>
2024-11-04 17:54:10 -0600

    p2pool: use a thirdparty monero node

--- modules/p2pool.nix
+++ modules/p2pool.nix
@@ -61,29 +61,9 @@ in
 
     })
     (mkIf cfg.serve {
-      users.groups.p2pool = { };
-      users.users.p2pool = {
-        group = "p2pool";
-        isSystemUser = true;
-      };
-
-      services.monero = {
-        enable = true;
-        priorityNodes = [
-          "p2pmd.xmrvsbeast.com:18080"
-          "nodes.hashvault.pro:18080"
-          "xmr1.julias.zone:18089"
-        ];
-        extraConfig = ''
-          zmq-pub=tcp://127.0.0.1:18083
-          out-peers=500
-          in-peers=800
-        '';
-      };
 
       # Open firewall ports
       networking.firewall.allowedTCPPorts = [
-        18080 # Monero P2P port
         37889 # p2pool
         37888 # p2pool mini
       ];
@@ -93,10 +73,13 @@ in
         wantedBy = [ "multi-user.target" ];
         description = "P2Pool instance";
         serviceConfig = {
+          DynamicUser = true;
           User = "p2pool";
-          Group = "p2pool";
           Restart = "always";
-          ExecStart = "${pkgs.p2pool}/bin/p2pool --host 127.0.0.1 --wallet  ${cfg.walletAddress}";
+          WorkingDirectory = "/var/lib/p2pool";
+          StateDirectory = "p2pool";
+          StateDirectoryMode = "0700";
+          ExecStart = "${pkgs.p2pool}/bin/p2pool --host node.richfowler.net --rpc-port 18089 --zmq-port 18084 --wallet  ${cfg.walletAddress}";
         };
 
       };
