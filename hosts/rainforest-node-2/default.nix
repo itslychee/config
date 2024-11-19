d08f76c870281773c05cc223a0acad90ad3a8f2f
itslychee <itslychee@protonmail.com>
2024-11-18 23:50:33 -0600

    rainforest-node-2: set up netbox

--- hosts/rainforest-node-2/default.nix
+++ hosts/rainforest-node-2/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }:
@@ -16,11 +17,47 @@
     hostKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGSuccGN1fYQQqKWK5Eg+Ldj7H1a6LDIJsXxI3646Jgg";
   };
 
+  deployment.keys.netbox-secret = {
+    destDir = "/var/lib/secrets/netbox";
+    user = "netbox";
+    group = "netbox";
+    keyCommand = [
+      "gpg"
+      "--decrypt"
+      (toString ../../secrets/netbox-secret.gpg)
+    ];
+  };
+
   hey.remote.builder = {
     enable = true;
     maxJobs = 20;
     speedFactor = 100;
   };
+
+  services.netbox = {
+    enable = true;
+    package = pkgs.netbox;
+    settings = {
+      CSRF_TRUSTED_ORIGINS = [ "http://rainforest-node-2" ];
+    };
+    secretKeyFile = config.deployment.keys.netbox-secret.path;
+  };
+
+  services.nginx.enable = true;
+  services.nginx.virtualHosts."rainforest-node-2" = {
+    locations = {
+      "/" = {
+        proxyPass = "http://${config.services.netbox.listenAddress}:${toString config.services.netbox.port}";
+        extraConfig = ''
+          proxy_set_header X-Forwarded-Host $http_host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+      "/static/".alias = "${config.services.netbox.dataDir}/static/";
+    };
+  };
+  users.groups.netbox.members = [ config.services.nginx.user ];
 
   services.consul.extraConfig = {
     server = true;
