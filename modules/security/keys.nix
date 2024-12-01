7719c508c722204c30925457d2706323a05ee184
itslychee <itslychee@protonmail.com>
2024-11-30 19:58:21 -0600

    remove unused keys

--- modules/security/keys.nix
+++ modules/security/keys.nix
@@ -10,23 +10,14 @@ in
     type = attrsOf (attrsOf (listOf str));
   };
   config.hey.keys = {
-    # me
-    lychee =
-      let
-        hearthKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0oSTfGZYuBmADdQofLJm22dcIFAUaos048sayYp5/J";
-      in
-      {
-        # "Tags" for finer permissions
-        secrets = [ hearthKey ];
-        ssh = [
-          hearthKey
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHXeFJBxjG2NgeKr4l58KIp7lPf/pUeYD/4bYVapuump phone"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILv8G52B8ANAEczyOLd6N15pmasoVde1I9pXajQOeUL5 openpgp:0xF2E43235"
-        ];
-        deployment = [
-          hearthKey
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILv8G52B8ANAEczyOLd6N15pmasoVde1I9pXajQOeUL5 openpgp:0xF2E43235"
-        ];
-      };
+    lychee = {
+      ssh = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHXeFJBxjG2NgeKr4l58KIp7lPf/pUeYD/4bYVapuump phone"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILv8G52B8ANAEczyOLd6N15pmasoVde1I9pXajQOeUL5 openpgp:0xF2E43235"
+      ];
+      deployment = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILv8G52B8ANAEczyOLd6N15pmasoVde1I9pXajQOeUL5 openpgp:0xF2E43235"
+      ];
+    };
   };
 }
