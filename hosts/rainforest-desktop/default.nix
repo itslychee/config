c4899cf93def442aa325525b105c33d39f29bb9f
itslychee <itslychee@protonmail.com>
2024-12-17 08:28:51 -0600

    rainforest-desktop: add vesktop

--- hosts/rainforest-desktop/default.nix
+++ hosts/rainforest-desktop/default.nix
@@ -17,6 +17,7 @@
 
   environment.systemPackages = [
     pkgs.minicom
+    pkgs.vesktop
   ];
 
   hey.hostKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGmZ4ydKauxo7XWxs7KBscNs+467oyFtC9jIevfiZOzv";
