68c2cbe60652a0b8cfcbb55a8221c15b44255df8
itslychee <itslychee@protonmail.com>
2025-04-09 09:56:22 -0500

    gns3 stuff

--- hosts/rainforest-desktop/default.nix
+++ hosts/rainforest-desktop/default.nix
@@ -1,46 +1,48 @@
 { pkgs, ... }:
 {
-  hey.github.enable = false;
   boot = {
     loader.systemd-boot.enable = true;
     binfmt.emulatedSystems = [ "aarch64-linux" ];
   };
   virtualisation.docker.enable = true;
 
-  services.printing = {
-    enable = true;
-    drivers = [ pkgs.hplipWithPlugin ];
-  };
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
   environment.systemPackages = [
     pkgs.minicom
     pkgs.libreoffice
+    pkgs.gns3-gui
     (pkgs.vesktop.override {
       withMiddleClickScroll = true;
       withSystemVencord = true;
     })
   ];
+  hey = {
+    github.enable = false;
 
-  hey.hostKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGmZ4ydKauxo7XWxs7KBscNs+467oyFtC9jIevfiZOzv";
-  hey.graphical.games = true;
-  services.desktopManager.plasma6 = {
-    enable = true;
-    enableQt5Integration = true;
-  };
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
+    hostKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGmZ4ydKauxo7XWxs7KBscNs+467oyFtC9jIevfiZOzv";
+    graphical.games = true;
 
-  hey.remote.builder = {
-    enable = true;
-    maxJobs = 5;
-    speedFactor = 35;
+    remote.builder = {
+      enable = true;
+      maxJobs = 5;
+      speedFactor = 35;
+    };
+  };
+  services = {
+    printing = {
+      enable = true;
+      drivers = [ pkgs.hplipWithPlugin ];
+    };
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+    desktopManager = {
+      plasma6.enable = true;
+      plasma6.enableQt5Integration = true;
+    };
+    displayManager.sddm.enable = true;
+    displayManager.sddm.wayland.enable = true;
   };
 
   networking.networkmanager.enable = true;
--- hosts/rainforest-node-1/default.nix
+++ hosts/rainforest-node-1/default.nix
@@ -1,52 +1,40 @@
 {
   config,
-  lib,
   ...
 }:
 {
   boot.loader.systemd-boot.enable = true;
-  hey.hostKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHiCax0o/1zd+Ry7impZRDbOn4F3ife/A1HhS0EYh1KH root@rainforest-node-2"
-  ];
+  hey = {
+    hostKeys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHiCax0o/1zd+Ry7impZRDbOn4F3ife/A1HhS0EYh1KH root@rainforest-node-2"
+    ];
 
-  hey.github.enable = true;
-  hey.remote.builder = {
-    enable = true;
-    maxJobs = 40;
-    speedFactor = 105;
+    github.enable = true;
+    remote.builder = {
+      enable = true;
+      maxJobs = 40;
+      speedFactor = 105;
+    };
   };
 
   networking.firewall.interfaces.${config.services.tailscale.interfaceName} = {
-    allowedTCPPorts = [ 9050 ];
+    allowedTCPPorts = [
+      9050
+      3080
+    ];
   };
 
-  services.promtail.configuration.scrape_configs = lib.singleton {
-    job_name = "syslog-ng";
-    syslog = {
-      listen_address = "127.0.0.1:1514";
-      label_structured_data = true;
-      labels.job = "syslog";
-    };
-    relabel_configs = lib.singleton {
-      source_labels = [ "__syslog_message_hostname" ];
-      target_label = "host";
-    };
-  };
-
-  networking.firewall.allowedUDPPorts = [ 514 ];
-
-  services.syslog-ng = {
+  services.gns3-server = {
     enable = true;
-    extraConfig = ''
-      destination d_loki {
-        syslog("127.0.0.1" transport("tcp") port(1514));
-      };
-      log {
-          source {
-            network();
-          };
-          destination(d_loki);
-      };
-    '';
+    ubridge.enable = true;
+    dynamips.enable = true;
+    vpcs.enable = true;
+
   };
+
+  networking.firewall.allowedUDPPorts = [
+    514
+    3080
+  ];
+
 }
