544f0cc591c696e20865e9b48dee054dc86e6b8b
itslychee <itslychee@protonmail.com>
2025-04-14 12:39:28 -0500

    rainforest-node-2: enable github runners

--- hosts/rainforest-node-2/default.nix
+++ hosts/rainforest-node-2/default.nix
@@ -9,6 +9,8 @@
 
   hey.hostKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGSuccGN1fYQQqKWK5Eg+Ldj7H1a6LDIJsXxI3646Jgg";
 
+  hey.github.enable = true;
+
   hey.remote.builder = {
     enable = true;
     maxJobs = 20;
