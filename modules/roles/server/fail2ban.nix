d55e6cd1254fad36791f7a3e5793954852785366
itslychee <itslychee@protonmail.com>
2025-04-15 11:04:01 -0500

    tailscale: add prefix IPv6 ULA

--- modules/roles/server/fail2ban.nix
+++ modules/roles/server/fail2ban.nix
@@ -1,7 +1,4 @@
 {
-  ...
-}:
-{
   services.fail2ban = {
     enable = true;
     maxretry = 5;
@@ -9,6 +6,7 @@
       "::1"
       "127.0.0.1"
       "100.0.0.0/8"
+      "fd7a:115c:a1e0::/48"
     ];
     bantime = "24h";
   };
