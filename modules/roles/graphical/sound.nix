a7a8a58da3612cd5caf70f98faa2373703b6c85e
itslychee <itslychee@protonmail.com>
2025-05-19 08:42:58 -0500

    hardware -> services.pulseaudio.enable

--- modules/roles/graphical/sound.nix
+++ modules/roles/graphical/sound.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   security.rtkit.enable = true;
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     pulse.enable = true;
