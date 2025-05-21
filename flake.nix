d4181327446a79eab8766c651df2d9e7d9a3ec8c
itslychee <itslychee@protonmail.com>
2025-05-21 13:57:49 -0500

    remove spicetify

--- flake.nix
+++ flake.nix
@@ -5,9 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     factorio.url = "github:MichailiK/nixpkgs/389365";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nextcloud-caddy.url = "github:onny/nixos-nextcloud-testumgebung/56a5379b83ea9c03d4d16daf27ac91e1ba6b020f";
-    # nextcloud-caddy.flake = false;
-    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
   };
   outputs =
     {
--- modules/roles/graphical/default.nix
+++ modules/roles/graphical/default.nix
@@ -1,36 +1,13 @@
 {
   inputs,
   pkgs,
-  lib,
   ...
 }:
-let
-  inherit (inputs) spicetify-nix;
-  spkgs = spicetify-nix.legacyPackages.${pkgs.system};
-in
 {
 
   deployment.tags = [ "graphical" ];
   hey.roles.graphical = true;
 
-  imports = lib.fileset.toList (lib.fileset.difference ./. ./default.nix) ++ [
-    spicetify-nix.nixosModules.default
-  ];
-
-  programs.spicetify = {
-    enable = true;
-    enabledExtensions = builtins.attrValues {
-      inherit (spkgs.extensions)
-        adblock
-        playlistIcons
-        fullAlbumDate
-        hidePodcasts
-        trashbin
-        shuffle
-        autoSkipVideo
-        ;
-    };
-  };
   programs.wireshark.package = pkgs.wireshark-qt;
   programs.gnupg.agent = {
     enable = true;
