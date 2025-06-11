5c8a5a0508ce0a04b60789be23c5111a21fef4f1
itslychee <itslychee@protonmail.com>
2025-06-11 12:37:15 -0500

    remove rainforest-node-1 and rainforest-node-2

--- hive.nix
+++ hive.nix
@@ -42,14 +42,6 @@ in
     ./modules/roles/graphical
     { system.stateVersion = "24.05"; }
   ];
-  rainforest-node-1.imports = [
-    ./modules/roles/server
-    { system.stateVersion = "24.05"; }
-  ];
-  rainforest-node-2.imports = [
-    ./modules/roles/server
-    { system.stateVersion = "24.05"; }
-  ];
   kaycloud.imports = [
     ./modules/roles/server
     { system.stateVersion = "25.05"; }
