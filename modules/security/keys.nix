876daed276f07e91128ead64cea6c2df13f58a77
itslychee <itslychee@proton.me>
2026-05-03 19:34:56 -0500

    fix security key being not present

--- modules/security/keys.nix
+++ modules/security/keys.nix
@@ -12,11 +12,10 @@ in
   config.hey.keys = {
     lychee = {
       ssh = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHXeFJBxjG2NgeKr4l58KIp7lPf/pUeYD/4bYVapuump phone"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILv8G52B8ANAEczyOLd6N15pmasoVde1I9pXajQOeUL5 openpgp:0xF2E43235"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHcRclGevnYAIGPNsMrJ/kk2NaE3/HPtiFRnRY6FSMIR openpgp:0x6386F9F1"
       ];
       deployment = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILv8G52B8ANAEczyOLd6N15pmasoVde1I9pXajQOeUL5 openpgp:0xF2E43235"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHcRclGevnYAIGPNsMrJ/kk2NaE3/HPtiFRnRY6FSMIR openpgp:0x6386F9F1"
       ];
     };
   };
